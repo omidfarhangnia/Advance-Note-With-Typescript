877d345751704c6b54a3bbaaa08e1bc8c9a596f4
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-07-02 21:30:33 +0330

    starting the structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { useState } from 'react';
 import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./Component/Home";
+import New from "./Component/New";
+import Show from "./Component/Show";
+import Edit from "./Component/Edit";
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<h1>hello there</h1>} />
-        <Route path='/new' element={<h1>new</h1>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/new' element={<New />} />
         <Route path='/:id'>
-            <Route index element={<h1>show</h1>} />
-            <Route path='edit' element={<h1>edit</h1>} />
+            <Route index element={< Show/>} />
+            <Route path='edit' element={<Edit />} />
         </Route>
         <Route path='*' element={<Navigate to="/" />} />
       </Routes>
---
+++ src/Component/Home.tsx
@@ -0,0 +1,22 @@
+export default function Home() {
+    return (
+        <>
+            <div>
+                <h1>Note</h1>
+
+                <div>
+                    <button>create</button>
+                    <button>edit tags</button>
+                </div>
+            </div>
+            <div>
+                <from>
+                    <div>
+                        <label>title</label>
+                        <input type="text" />
+                    </div>
+                </from>
+            </div>
+        </>
+    )
+}
