c4c0f6604189f5335bca409052b8ad18a128372b
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-07-04 11:35:02 +0330

    adding filter to note page

--- src/Component/Home.tsx
+++ src/Component/Home.tsx
@@ -1,7 +1,7 @@
 import { PiGhostFill } from "react-icons/pi";
 import { note } from "./projectTypes";
 import { Link } from "react-router-dom";
-import { ButtonHTMLAttributes, useRef } from "react";
+import { ButtonHTMLAttributes, useRef, useState } from "react";
 
 type homeProps = {
   notes: note[];
@@ -9,6 +9,8 @@ type homeProps = {
 };
 
 export default function Home({ notes, tags }: homeProps) {
+  const [chosenTitle, setChosenTitle] = useState<string>("");
+  
   return (
     <>
       <div className="w-full h-screen bg-gray-400/30 flex gap-[7vh] flex-col px-[10vw] py-[5vh]">
@@ -39,6 +41,9 @@ export default function Home({ notes, tags }: homeProps) {
                 type="text"
                 className="p-3 text-[20px] bg-transparent outline-none border-2 border-solid border-blue-400 rounded-lg"
                 spellCheck={false}
+                onChange={(e) => {
+                  setChosenTitle(e.target.value);
+                }}
               />
             </div>
             <div className="w-[43%] flex flex-col gap-3 p-3">
@@ -63,30 +68,49 @@ export default function Home({ notes, tags }: homeProps) {
         </div>
         <div className="border-2 flex flex-wrap overflow-y-scroll items-center justify-around border-solid border-blue-600 rounded-xl min-h-[40vh] gap-5 p-5">
           {notes.length > 0 ? (
-            notes.map((note, index) => (
-              <Link
-                to={`/${note.id}`}
-                key={index}
-                className="w-[30%] flex flex-col gap-2 py-3 rounded-2xl items-center border-2 border-solid border-blue-400"
-              >
-                <div className="font-bold">{note.title}</div>
-                <div>
-                  {note.tags.map((tag) => (
-                    <span className="bg-blue-400 py-1 px-2 text-[13px] font-bold text-white rounded-full">
-                      {tag}
-                    </span>
-                  ))}
-                </div>
-              </Link>
-            ))
+            chosenTitle.length === 0 ? (
+              notes.map((note, index) => <NoteCards note={note} key={index} />)
+            ) : (
+              notes
+                .filter((note) => note.title.startsWith(chosenTitle))
+                .map((note, index) => <NoteCards note={note} key={index} />)
+            )
           ) : (
-            <div className="flex flex-col justify-center items-center gap-3">
-              <PiGhostFill size={90} className="opacity-70 animatedGhost" />
-              <span className="text-[25px] capitalize">there is no note</span>
-            </div>
+            <EmptyNoteList />
           )}
         </div>
       </div>
     </>
+  );
+}
+
+type NoteCardsProps = {
+  note: note;
+};
+
+function NoteCards({ note }: NoteCardsProps) {
+  return (
+    <Link
+      to={`/${note.id}`}
+      className="w-[30%] flex flex-col gap-2 py-3 rounded-2xl items-center border-2 border-solid border-blue-400"
+    >
+      <div className="font-bold">{note.title}</div>
+      <div>
+        {note.tags.map((tag) => (
+          <span className="bg-blue-400 py-1 px-2 text-[13px] font-bold text-white rounded-full">
+            {tag}
+          </span>
+        ))}
+      </div>
+    </Link>
+  );
+}
+
+function EmptyNoteList() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-3">
+      <PiGhostFill size={90} className="opacity-70 animatedGhost" />
+      <span className="text-[25px] capitalize">there is no note</span>
+    </div>
   );
 }
